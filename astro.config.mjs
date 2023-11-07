6cd5085562121426abea6ccf9bbd48fedc57090c
Angel Arcia <aarcia@jc-innovation.com>
2023-11-07 11:14:13 -0600

    feat: build to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jc-innovation-ti.github.io",
-  base: "/my-repo",
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
 });
